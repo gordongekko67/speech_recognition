0623a95a64aa0ce11142b0f79877b01ea6a3a502
gordongekko67 <34415973+gordongekko67@users.noreply.github.com>
2020-05-30 10:30:56 +0200

    Add files via upload

---
+++ sp_recognition_ok.py
@@ -0,0 +1,98 @@
+import pyautogui
+import speech_recognition as sr
+from PIL.Image import Image
+from imageai.Detection import ObjectDetection
+import os
+import shutil
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+import numpy as np
+
+import paho.mqtt.client as mqtt
+import time
+
+
+
+# referenze libreria: https://github.com/Uberi/speech_recognition/blob/master/reference/library-reference.rst#speech-recognition-library-reference
+
+recognizer_instance = sr.Recognizer() # Crea una istanza del recognizer
+
+text = ""
+
+def on_connect(client, userdata, flags, rc):
+    print("connected with code "+ str(rc))
+    # substrice topic
+    client.subscribe("Topic/#")
+
+def on_message(client, userdata, msg):
+    print(str(msg.payload))
+
+
+
+def avanti():
+    print("robot avanti")
+
+def elaborazione():
+    
+    execution_path = os.getcwd()
+    detector = ObjectDetection()
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
+    detector.loadModel()
+    detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "opencv0.png"),
+                                                 output_image_path=os.path.join(execution_path, "imagenew.jpg"))
+    for eachObject in detections:
+        print(eachObject["name"], " : ", eachObject["percentage_probability"])
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.username_pw_set("fkjqkoul", "wK0aUWpQWS35")
+    client.connect("tailor.cloudmqtt.com", 16434, 60)
+
+    client.loop_forever()
+
+    time.sleep(1)
+    while True:
+        client.publish("Tutorial2", "Getting started with MQTT TEST")
+        time.sleep(15)
+
+    client.loop_stop()
+    client.disconnect()
+
+
+
+
+
+
+def riconosciimmagine():
+    camera = cv2.VideoCapture(0)
+    for i in range(1):
+        return_value, image = camera.read()
+        cv2.imwrite('opencv' + str(i) + '.png', image)
+        elaborazione()
+
+def indietro():
+    print("robot indietro")
+    riconosciimmagine()
+
+
+while(text != "esci"):
+    with sr.Microphone() as source:
+        recognizer_instance.adjust_for_ambient_noise(source)
+        print("Sono in ascolto... parla pure!")
+        audio = recognizer_instance.listen(source)
+        print("Ok! sto ora elaborando il messaggio!")
+    try:
+        text = recognizer_instance.recognize_google(audio, language="it-IT")
+        print("Google ha capito: \n", text)
+        if (text == "esci"):
+             break
+        if (text =="avanti"):
+            avanti()
+        if (text =="indietro"):
+            indietro()
+    except Exception as e:
+         print(e)
